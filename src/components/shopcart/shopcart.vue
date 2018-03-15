76ff4f707ee1a30edece75b1f94d44da737ceeab
fary <910146725@qq.com>
2018-03-15 10:56:07 +0800

    update shopcart component

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -5,9 +5,10 @@
               <div class="logo-wrapper">
                   <div class="logo">
                       <i class="icon-shopping_cart"></i>
+                      <div class="num">1</div>
                   </div>
               </div>
-              <div class="price">￥0</div>
+              <div class="price" :class="{highlight:totalPrice>0}">￥{{totalPrice}}</div>
               <div class="desc">另需配送费{{deliveryPrice}}元</div>
           </div>
           <div class="content-right">
@@ -25,7 +26,10 @@
             selectedFoods:{
                 type:Array,
                 default(){
-                    return [];
+                    return [{
+                        price:10,
+                        count:1
+                    }];
                 }
             },
             delivePrice:{
@@ -34,6 +38,22 @@
             minPrice:{
                 type:Number
             }
+        },
+        computed:{
+            totalPrice(){
+                let total=0;
+                this.selectedFoods.forEach((food)=>{
+                    total+=food.price*food.count;
+                });
+                return total;
+            },
+            totalCount(){
+                let count=0;
+                this.selectedFoods.forEach((food)=>{
+                    count+=food.count;
+                });
+                return count;
+            }            
         }
     }
 </script>
@@ -55,6 +75,19 @@
          .content-left
            flex:1
            .logo-wrapper
+             .num
+               position :absolute
+               top :0
+               right :0
+               width :24px
+               height :16px
+               text-align :center
+               border-radius :16px
+               font-size :9px
+               font-size :700
+               color:#fff
+               background :rgb(240,20,20)
+               box-shadow 0 4px 8px 0 rgba(0,0,0,0.4)
              display:inline-block
              position:relative
              top:-10px
