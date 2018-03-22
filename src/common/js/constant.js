cb0f4ac364c01007470ba714103adac2e0f684a9
fary <910146725@qq.com>
2018-03-22 17:23:10 +0800

    add simulator mode

--- src/common/js/constant.js
+++ src/common/js/constant.js
@@ -6,14 +6,22 @@ const RatingType={
      NEGATIVE:1,
      ALL:2
   };
+
+
 const host="localhost";
 const port="8080"
-
+const isSimlatorMode=true;
  let Api={
    getSeller(sellerId){
+     if(isSimlatorMode){
+       return "/api/seller";
+     }
      return `http://${host}:${port}/api/Seller/${sellerId}`;
    },
    getGoods(sellerId){
+    if(isSimlatorMode){
+      return "/api/goods";
+    }    
     return `http://${host}:${port}/api/goods/${sellerId}`;
   }
  } 
