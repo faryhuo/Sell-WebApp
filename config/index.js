6352c4b05388acf03b5c9c4c419e6f9362133c8f
fary <910146725@qq.com>
2018-07-27 11:00:34 +0800

    Fary:change config

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'sellappvue/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/sellappvue',
 
     /**
      * Source Maps
